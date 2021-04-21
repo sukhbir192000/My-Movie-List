89a9db66d6575acacd786390c385dbad5d4f2434
puneetkalsi99 <puneetkalsi99@gmail.com>
2021-04-21 14:17:49 +0530

    task

--- MML/src/java/servlets/ShowController.java
+++ MML/src/java/servlets/ShowController.java
@@ -8,7 +8,6 @@ package servlets;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +16,6 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Puneet
  */
-@WebServlet(name = "ShowController", urlPatterns = {"/ShowController"})
 public class ShowController extends HttpServlet {
 
     /**
